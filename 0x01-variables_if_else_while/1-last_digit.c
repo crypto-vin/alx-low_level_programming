312419fb3669da5c5c0b2cd313b2e7c2c94bf1f2
vin <44378566+crypto-vin@users.noreply.github.com>
2022-12-11 18:56:55 +0300

    minor fix on task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_n);
 	} else if (last_n == 0)
 	{
-		printf("Last digit of %d is %d and is o\n", n, last_n);
+		printf("Last digit of %d is %d and is 0\n", n, last_n);
 	} else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_n);
