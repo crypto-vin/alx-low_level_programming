8d7a07e0f35aee7f8493f719873c2df5b23804f5
vin <44378566+crypto-vin@users.noreply.github.com>
2022-12-23 22:25:25 +0300

    fixed Betty coding style

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,5 @@
 #include "main.h"
-
-/*
+/**
  * _strncpy - copy a string
  * @dest: input value
  * @src: input value
@@ -8,17 +7,15 @@
  *
  * Return: dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
-	
+
 	while (j < n && src[j] != '\0')
 	{
 		dest[j] = src[j];
 		j++;
 	}
-
 	while (j < n)
 	{
 		dest[j] = '\0';
