e37d8dc26a6464d251f32ccfcef98a79504b8991
Vincent Munyalo <vmulwah@gmail.com>
2022-12-23 11:45:20 -0800

    added soln to task #4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * reverse_array - reverses an array
+ *
+ * @a: array to reverse
+ * @n: size of array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int t = 0;
+
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
