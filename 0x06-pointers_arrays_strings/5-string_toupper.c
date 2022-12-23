c42f1e9eb02aa793efb9bea080d02b9eaf74d5ea
vin <44378566+crypto-vin@users.noreply.github.com>
2022-12-23 22:56:40 +0300

    sorted Betty coding style

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - alter all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n value
+ */
+
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
