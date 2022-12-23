e78946b77a47be2a3c7a1373ec30061cd759e62e
Vincent Munyalo <vmulwah@gmail.com>
2022-12-23 09:52:38 -0800

    fixed task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
-	while (dest[i] != '\0')
+	for (j = 0; dest[j] != '\0'; j++)
 	{
-		i++;
+		/* increment j*/
 	}
 
-	while (src[j] != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[j + i] = src[i];
 	}
 
-	dest[i] = '\0';
+	dest[i + j] = '\0';
 	return (dest);
 }
