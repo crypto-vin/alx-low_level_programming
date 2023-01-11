aaf25e7bb832c544dcda2ae6a44b920792232aa4
vin <44378566+crypto-vin@users.noreply.github.com>
2023-01-11 22:51:38 +0300

    fixed betty style coding

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,37 +1,29 @@
-#include "main.h"
-		
-#include <stdlib.h>
-		
-/**
-		
+#include "main.h"	
+#include <stdlib.h>	
+/**	
  * argstostr - main entry
-		
  * @ac: int input
-		
  * @av: double pointer array
-		
- * Return: 0
-		
+ * Return: 0	
  */
-		
+
 char *argstostr(int ac, char **av)
-		
-{	
+{
 	int i, n, r = 0, l = 0;
 	char *str;
 	
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	
-	for (i = 0; i < ac; i++)	
+	for (i = 0; i < ac; i++)
 	{
 		for (n = 0; av[i][n]; n++)
 			l++;
 	}
-		
+	
 	l += ac;
 	
-	str = malloc(sizeof(char) * l + 1);	
+	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
 	
@@ -41,7 +33,6 @@ char *argstostr(int ac, char **av)
 		{
 			str[r] = av[i][n];
 			r++;
-		
 		}
 		
 		if (str[r] == '\0')
@@ -50,5 +41,5 @@ char *argstostr(int ac, char **av)
 		}
 	}
 	
-	return (str);	
+	return (str);
 }
