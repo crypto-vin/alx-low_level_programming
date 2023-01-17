da31e47b392e5d1499ad79c12645b3f3bde12df6
Vincent Munyalo <vmulwah@gmail.com>
2023-01-17 10:46:57 -0800

    added task #0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,13 @@
+0x0D. C - Structures, typedef
+
+Tasks:
+
+0. Define a new type struct dog with the following elements:
+
+
+
+name, type = char *
+
+age, type = float
+
+owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ */
+
+struct dog 
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, char *owner,  float age);
+void free_dog(dog_t *d);
+
+#endif
