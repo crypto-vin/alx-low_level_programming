9c7fc6af2c2d4c25297adef0bf997bccb0d259e3
Vincent Munyalo <vmulwah@gmail.com>
2023-01-17 07:37:21 -0800

    added tasks #1 to #4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main file that returns the file name
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
