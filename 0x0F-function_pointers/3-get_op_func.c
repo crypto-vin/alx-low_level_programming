3dbbb9b55657b7efffe4bf00689df5e5c38c5c28
vin <44378566+crypto-vin@users.noreply.github.com>
2023-01-20 21:46:52 +0300

    ADDED NEW FILE

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,54 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "3-calc.h"
-/**
- * op_add - adds 5 functions
- * @a: int a
- * @b: int b
- * Return: sum of a and b
- */
-int op_add(int a, int b)
-{
-	return (a + b);
-}
-/**
- * op_sub - subtracts
- * @a: int a
- * @b: int b
- * Return: difference
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
- * op_mul - multiplies
- * @a: int a
- * @b: int b
- * Return: multipliy
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
- * op_div - division
- * @a: int a
- * @b: int b
- * Return: division
- */
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
- * op_mod - modulos
- * @a: int a
- * @b: int b
- * Return: modulo of ints
- */
 
-int op_mod(int a, int b)
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
+ *
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
+ */
+int (*get_op_func(char *s))(int, int)
 {
-	return (a % b);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+
+
+	int i = 0;
+
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+
+	return (ops[i].f);
 }
