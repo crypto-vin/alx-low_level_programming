c07480edb64ba42a825dae703a69c7861b24c548
vin <44378566+crypto-vin@users.noreply.github.com>
2023-01-20 21:42:00 +0300

    sorted calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,16 @@
-#ifndef _3_CALC_H
-#define _3_CALC_H
+#ifndef CALC_H
+#define CALC_H
+/**
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -7,16 +18,6 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+
 
 #endif
